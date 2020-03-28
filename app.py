7f3f59d5f7c7982498eadaeafef49d0410cd1372
Ravi Patel <ravi@kheironmed.com>
2020-03-28 09:58:05 -0700

    bugfix

--- app.py
+++ app.py
@@ -52,7 +52,5 @@ def add_generic():
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    if __name__ == '__main__':
-
     app.wsgi_app = LoggingMiddleware(app.wsgi_app)
     app.run(host='0.0.0.0', port=port)
